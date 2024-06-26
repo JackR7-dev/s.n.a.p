a3d416fea42b811c20df86e0b2c4bb8eac1ff078
JackR7-dev <netraiden@gmail.com>
2024-06-26 12:33:05 +0200

    fix comments

--- main.py
+++ main.py
@@ -25,14 +25,12 @@ output_path = "E:\Files\Programming\Projects\S.N.A.P. (Smart Neural Art Poster)\
 # getting the list of elements inside OUTPUT folder
 dir_list = os.listdir(output_path)
 
-# function that generate images based on prompt parameter
+# function that generate images based on prompt parameter. It cycle the image list directory so it can delete previuous generated images.
+# Then it run a command line to generate images and save it in the OUTPUT folder
 def generate_images(prompt):
-    # for loop in OUTPUT folder to delete older generated pics
     for img in dir_list:
         os.remove(output_path + "\\" + img)
-    # definition of a terminal command that generates images
     command = f'python -m BingImageCreator --prompt "{prompt}" -U "{bing_api}"'
-    # run the command on the terminal
     os.system(command)
     return os.listdir("OUTPUT")
 
@@ -46,21 +44,20 @@ def verify_images(dir_list):
             os.remove(output_path + "\\" + img)
     return True
 
-
+# function that get a random pic inside OUTPUT folder, upload the pic and get the media_id that will be used to create the tweet
 def upload_img_and_return_id():
-    # getting a random number for selecting a picture inside OUTPUT folder
     random_number = random.randint(0, len(dir_list) - 1)
     img = dir_list[random_number]
-    # getting complete image path
     img_path = output_path + "\\" + img
-    # uploading the pic on twitter and getting the specific media id
     media_id = api.media_upload(filename=img_path).media_id_string
     return media_id
 
 generate_images("A super hero is fighting a villain on the sea")
-media_id = upload_img_and_return_id()
 verify_images(dir_list)
+media_id = upload_img_and_return_id()
 
-# creating the tweet and passing the media id as a list. The function create_tweet require a list when passing a media_ids parameter
-# client.create_tweet(media_ids=[media_id])
+# creating the tweet and passing the media id as a list even if it's only one string. 
+# The function create_tweet require a list when passing a media_ids parameter
+client.create_tweet(media_ids=[media_id])
+
 print("Tweeted")
