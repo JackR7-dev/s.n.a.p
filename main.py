ca90e96b68045e0859284431c63d62e0b77765ec
JackR7-dev <netraiden@gmail.com>
2024-06-26 20:34:33 +0200

    fix

--- main.py
+++ main.py
@@ -9,13 +9,8 @@ import img_generator
 import img_upload
 from PIL import Image
 
-output_path = "E:\Files\Programming\Projects\S.N.A.P. (Smart Neural Art Poster)\OUTPUT"
-# getting the list of elements inside OUTPUT folder
-dir_list = os.listdir(output_path)
-
-
 img_generator.generate_images("A panda eating a rock")
-img_generator.verify_images(dir_list)
+img_generator.verify_images(img_generator.dir_list)
 media_id = img_upload.upload_img_and_return_id()
 
 # creating the tweet and passing the media id as a list even if it's only one string. 
