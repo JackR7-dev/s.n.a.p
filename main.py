3508a7dbb11fe7ef8c0ec6a41ed56a77a7c87cab
JackR7-dev <netraiden@gmail.com>
2024-06-26 12:17:52 +0200

    fix + verify images function

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import os
 import random
 import config
+from PIL import Image
 
 bing_api = '1G3iWtGpFuD8Yj1bItIS4D7XbwijaR94Hs7ByeFi9IVeeDnNTEyfhIjvXkT1FmW0puys8uc9RcGHvPcqr0gElGrUst8W-DaHDDTYdcIyZPwRmrJKf8RTl4YX2eJ0aYk2Pe8Vod3LsALI7hFnAZMvVMlP7jP87pa9sZBSYm9xgpMXjKhn6ixCToaKWp031dWfY1_IEk-xDx-xWXKeq7anP_g'
 
@@ -27,27 +28,39 @@ dir_list = os.listdir(output_path)
 # function that generate images based on prompt parameter
 def generate_images(prompt):
     # for loop in OUTPUT folder to delete older generated pics
-    for n in dir_list:
-        os.remove(output_path + "\\" + n)
+    for img in dir_list:
+        os.remove(output_path + "\\" + img)
     # definition of a terminal command that generates images
     command = f'python -m BingImageCreator --prompt "{prompt}" -U "{bing_api}"'
     # run the command on the terminal
     os.system(command)
     return os.listdir("OUTPUT")
 
-def upload_img():
-    # for loop inside OUTPUT folder in order to get a random image name
-    for n in dir_list:
-        random_number = random.randint(0, len(dir_list) - 1)
-        img = dir_list[random_number]
+# function that check if the generated images is corrupted or can't be opened
+def verify_images(dir_list):
+    for img in dir_list:
+        try:
+            img_check = Image.open(output_path + "\\" + img)
+            img_check.verify()
+        except:
+            os.remove(output_path + "\\" + img)
+    return True
+
+
+def upload_img_and_return_id():
+    # getting a random number for selecting a picture inside OUTPUT folder
+    random_number = random.randint(0, len(dir_list) - 1)
+    img = dir_list[random_number]
     # getting complete image path
     img_path = output_path + "\\" + img
-
     # uploading the pic on twitter and getting the specific media id
     media_id = api.media_upload(filename=img_path).media_id_string
+    return media_id
 
 generate_images("A super hero is fighting a villain on the sea")
+media_id = upload_img_and_return_id()
+verify_images(dir_list)
 
 # creating the tweet and passing the media id as a list. The function create_tweet require a list when passing a media_ids parameter
-client.create_tweet(media_ids=[media_id])
+# client.create_tweet(media_ids=[media_id])
 print("Tweeted")
